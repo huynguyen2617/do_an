0789a3cae4fe0bd18f79b83bb95b0039ddffdeac
huynguyen2617 <huy.nv151697@sis.hust.edu.vn>
2021-10-15 14:12:56 +0700

    login1

--- test/main.js
+++ test/main.js
@@ -8,3 +8,45 @@ function myfunction(){
 		x = true;
     }
 }
+
+const data = [
+    {
+        username : "trung",
+        password : "123456",
+        full_name : "Le Trung"
+    },
+    {
+        username : "dung",
+        password : "dung123",
+        full_name : "Nguyen Xuan Dung"
+    },
+    {
+        username : "lanhuong",
+        password : "20102000",
+        full_name : "Lan Huong"
+    },
+    {
+        username : "admin",
+        password : "admin123",
+        full_name : "Admin"
+    }
+]
+
+
+const btn = document.getElementsByClassName("button");
+btn[0].addEventListener('click', ()=> {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    const user = data.find(el => el.username == username);
+    if(user){
+        if(user.password ==password){
+            alert("dang nhap thanh cong `${data.full_name}`");
+        }
+        else{
+            alert("sai mat khau");
+        }
+    }
+    else{
+        alert("sai username");
+    }
+});
