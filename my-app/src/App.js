f393b4ea9f9f3a5c2d47d9f66736e87b74d69aa3
huynguyen2617 <huy.nv151697@sis.hust.edu.vn>
2021-10-19 15:07:03 +0700

    react

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
-let stu = {
-  name: 'nam',
-  age: "34",
-  point: "9"
-}
+
 function App() {
-  return (
-    <div className="App">
-      <h1>Chào mứnginh viên: {stu.name}</h1>
-      <h1>tuoior: {stu.age}</h1>
-      <h1>Chào mứnginh viên: {stu.name}</h1>
-    </div>
+  return(
+    <h2>hello</h2>
   );
 }
 
